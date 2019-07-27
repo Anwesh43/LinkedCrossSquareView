f9c1cf5f238fd9e26807444b868ea4304f461f5c
Anwesh43 <anweshthecool0@gmail.com>
2019-07-27 11:36:26 +0530

    adding Canvas  extension method

--- crosssquareview/src/main/java/com/anwesh/uiprojects/crosssquareview/CrossSquareView.kt
+++ crosssquareview/src/main/java/com/anwesh/uiprojects/crosssquareview/CrossSquareView.kt
@@ -21,6 +21,7 @@ val scGap : Float = 0.05f
 val scDiv : Double = 0.51
 val foreColor : Int = Color.parseColor("#01579B")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val sqSizeFactor : Float = 2.8f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
@@ -31,3 +32,36 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawSquare(i : Int, sc : Float, size : Float, paint : Paint) {
+    val sci : Float = sc.divideScale(i, lines)
+    val sqSize : Float = size / sqSizeFactor
+    val x : Float = size * sci
+    save()
+    rotate(90f * i)
+    drawLine(0f, 0f, x, x, paint)
+    save()
+    translate(x, x)
+    drawRect(RectF(-sqSize / 2, -sqSize / 2, sqSize, sqSize), paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawCSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor 
+    save()
+    translate(w / 2, gap * (i + 1))
+    rotate(90f * sc2)
+    for (j in 0..(lines - 1)) {
+        drawSquare(j, sc1, size, paint)
+    }
+    restore()
+}
