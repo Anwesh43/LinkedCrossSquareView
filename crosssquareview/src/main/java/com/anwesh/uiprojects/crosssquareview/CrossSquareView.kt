f1bd2c62b80cd786e5d1b4fa5acae752fb79e974
Anwesh43 <anweshthecool0@gmail.com>
2019-07-27 11:20:21 +0530

    adding libraries and constants

--- crosssquareview/src/main/java/com/anwesh/uiprojects/crosssquareview/CrossSquareView.kt
+++ crosssquareview/src/main/java/com/anwesh/uiprojects/crosssquareview/CrossSquareView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.crosssquareview
 /**
  * Created by anweshmishra on 27/07/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 4
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val foreColor : Int = Color.parseColor("#01579B")
+val backColor : Int = Color.parseColor("#BDBDBD")
