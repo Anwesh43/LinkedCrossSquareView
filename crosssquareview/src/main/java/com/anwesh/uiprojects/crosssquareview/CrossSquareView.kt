28c5d04c8cdac338fd6378d4f55717388f039f01
Anwesh43 <anweshthecool0@gmail.com>
2019-07-27 11:55:43 +0530

    adding CSNode class

--- crosssquareview/src/main/java/com/anwesh/uiprojects/crosssquareview/CrossSquareView.kt
+++ crosssquareview/src/main/java/com/anwesh/uiprojects/crosssquareview/CrossSquareView.kt
@@ -130,4 +130,48 @@ class CrossSquareView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CSNode(var i : Int, val state : State = State()) {
+
+        private var next : CSNode? = null
+        private var prev : CSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = CSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CSNode {
+            var curr : CSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
