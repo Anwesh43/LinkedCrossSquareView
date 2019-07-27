5f995f42b767da5e47edce3896ebeaad6c83414e
Anwesh43 <anweshthecool0@gmail.com>
2019-07-27 12:02:40 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedcrosssquareview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcrosssquareview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedcrosssquareview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.crosssquareview.CrossSquareView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        CrossSquareView.create(this)
     }
 }
